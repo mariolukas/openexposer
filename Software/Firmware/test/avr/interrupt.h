401cc7c2f648340d14867062bdd7fc54b8a9ce00
Christoph Emonds <ich@christoph-emonds.de>
2016-02-26 00:08:46 +0100

    - removed no longer needed header file stubs

--- Software/Firmware/test/avr/interrupt.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by christoph on 2/23/16.
-//
-
-#ifndef OPENEXPOSER_INTERRUPT_H
-#define OPENEXPOSER_INTERRUPT_H
-
-#endif //OPENEXPOSER_INTERRUPT_H
--- Software/Firmware/test/util/delay.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by christoph on 2/23/16.
-//
-
-#ifndef OPENEXPOSER_DELAY_H
-#define OPENEXPOSER_DELAY_H
-
-#endif //OPENEXPOSER_DELAY_H
