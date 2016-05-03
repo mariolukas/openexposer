c59952710f0df8d7f15c97c8741fb1efbc9ed5ea
Christoph Emonds <ich@christoph-emonds.de>
2016-05-03 23:03:23 +0200

    - new version

--- Software/Firmware/MotorController/MotorController/MotorController.ino
+++ Software/Firmware/MotorController/MotorController/MotorController.ino
@@ -21,18 +21,29 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
     pinMode(OUTPUT, PA1);
-    pinMode(OUTPUT, PA2);
+    pinMode(OUTPUT, 11);
     pinMode(OUTPUT, PB2);
-    digitalWrite(PA2, HIGH);
-/*    CCP=0xd8;
+    digitalWrite(11, HIGH);
+//    digitalWrite(PB2, HIGH);
+    CCP=0xd8;
     CLKPR=(0<<CLKPS3) | (0<<CLKPS2) | (0<<CLKPS1) | (0<<CLKPS0);
 //    DDRA = (1 << PA2);//PA2 pin as an output
+/* PA2
     TOCPMSA1 = (1 << TOCC1S0);//TOCC1 linkage
     TOCPMCOE = (1 << TOCC1OE);//Enable PWMs
     ICR1 = (XTAL/PRESCALER/CLOCK) -1;
     OCR1A = (XTAL/PRESCALER/CLOCK) -1;
     TCCR1A = (1 << COM1A1) | (1 << COM1A0) | (0 << WGM13) | (1<< WGM12);
     TCCR1B = (0 << CS12) | (0 << CS11) | (1 << CS10);*/
+
+// PB2
+    TOCPMSA1 = (1 << TOCC7S0);//TOCC1 linkage
+    TOCPMCOE = (1 << TOCC7OE);//Enable PWMs
+    ICR1 = (XTAL/PRESCALER/CLOCK) -1;
+    OCR1A = (XTAL/PRESCALER/CLOCK) -1;
+    TCCR1A = (1 << COM1A1) | (1 << COM1A0) | (0 << WGM13) | (1<< WGM12);
+    TCCR1B = (0 << CS12) | (0 << CS11) | (1 << CS10);
+
 }
 
 // the loop function runs over and over again forever
